6b5d6097b4175d65e8b1763b35561739ff54099c
jpark2320 <jpark2320@gmail.com>
2020-11-07 04:16:35 -0500

    Init project with Next.js and Scss config

---
+++ src/components/App/index.js
@@ -0,0 +1,13 @@
+import styles from './App.module.scss';
+
+export default function App() {
+  return (
+    <div className={styles.app}>
+      <div className={styles.title}>
+        <h1>Gram Gallery</h1>
+        <h2>Your Pictures</h2>
+        <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit.</p>
+      </div>
+    </div>
+  );
+}
